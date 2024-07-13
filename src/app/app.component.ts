5f2d478b5824207f614872fa5c4e7461c6295c9d
SabaPangani <spangani37@gmail.com>
2024-07-13 18:40:46 +0400

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import { NavigationComponent } from "./components/navigation/navigation.componen
   standalone: true,
   imports: [HeaderComponent, FooterComponent, RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app';
